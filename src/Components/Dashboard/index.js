a97d4a474e398a5d36f05da15b9ae9c30fbfe1a1
Ketan Sunil Kulkarni <kketan482@gmail.com>
2023-11-30 02:05:12 +0530

    Dashboard Ready
    Dashboard Ready

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,6 +1,23 @@
-
-
+import { Button, Container, Stack, Typography } from "@mui/material";
+import Search from "../Search";
+import { Link } from "react-router-dom";
+import Tabel from "../Tabel";
 export default ()=>{
 
-    return (<div>This is Dasboard Page</div>)
+    return (<Container>
+        <Stack direction="column">
+        <Typography variant="h5" component="h5">
+            Total project : 22
+        </Typography>
+        <Stack  direction="row" justifyContent="space-between">
+            <Search/>
+           <Link to="/projectform"> <Button variant="contained">Create Project</Button></Link>
+
+        
+        </Stack>
+        <Typography component="div" sx={{marginTop:"30px"}}></Typography>
+        <Tabel/>
+        </Stack>
+        
+    </Container>)
 }
