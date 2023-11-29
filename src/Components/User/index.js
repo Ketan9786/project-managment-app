6f3c3cf62863f0aebadc7f5b0de10c43dee8c093
Ketan Sunil Kulkarni <kketan482@gmail.com>
2023-11-30 03:48:34 +0530

    User details page
    User details page

--- src/Components/User/index.js
+++ src/Components/User/index.js
@@ -1,4 +1,38 @@
-export default ()=>{
+import { Button, Container, Stack, Typography } from "@mui/material"
+import Tabel from "../Tabel";
+import { Link } from "react-router-dom";
 
-    return (<>This is User Page</>)
+export default () => {
+
+    return (<Container>
+        <Stack>
+            <Stack direction="row" justifyContent="space-between">
+
+
+                <Stack >
+                    <Typography variant="h5" component="div">
+                        Ketan Kulkarni
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        kketan482@gmail.com
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        Reopring to Sunil Kulkarni
+                    </Typography>
+
+                </Stack>
+                <Button variant="contained" sx={{height:"30px"}}>Edit</Button>
+            </Stack>
+            
+            <Stack direction="row" justifyContent="space-between">
+            <Typography variant="h5" component="div">
+                        Total Project : 22
+                    </Typography>
+                <Link to="/taskform"><Button variant="contained">New Task
+                </Button></Link>
+
+            </Stack>
+            <Tabel />
+        </Stack>
+    </Container>)
 }
