03c74598e38b1b83fea6c7829c29b1c5161e907b
Ketan Sunil Kulkarni <kketan482@gmail.com>
2023-11-30 23:41:43 +0530

    hash passwords decryption added
    hash passwords decryption added

--- backend/index.js
+++ backend/index.js
@@ -15,26 +15,33 @@ app.post('/login', (req, res) => {
     UsersModel.findOne({ email: email })
         .then(user => {
             if (user) {
-                if (user.password === password) {
-                    res.json("Success");
-                } else {
-                    res.json("Password is incorrect");
+               bcrypt.compare(password,user.password,(err,response)=>{
+                if(err){
+                    res.json("password is incorrect")
                 }
+                if(response){
+                    res.json("Sucess")
+                }
+               })
             } else {
                 res.json("No record found, please register");
             }
         })
-        .catch(err => res.status(500).json(err)); // Handle errors properly
+        .catch(err => res.status(500).json(err)); 
 });
 
 app.post('/register', (req, res) => {
-    
-    UsersModel.create(req.body)
+    const {fullName,password,email,mobileNumber,birthDate,gender}=req.body;
+    bcrypt.hash(password,10)
+    .then(hash=>{
+        UsersModel.create({fullName, password:hash ,email,mobileNumber,birthDate,gender})
         .then(user => res.json(user))
-        .catch(err => res.status(500).json(err)); // Handle errors properly
+        .catch(err => res.status(500).json(err));
+    })
+     
 });
 
-const PORT = process.env.PORT || 3001; // Use the environment port or default to 3001
+const PORT = process.env.PORT || 3001; 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
